3298d9343ead0f62c86411bd0a9b931b6ce7d7da
justdan111 <emmanuelngulube292@gmail.com>
2026-02-05 00:26:59 +0100

    Add JWT token generation utility and update dependencies

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"reflecta/internal/config"
+)
+
+func GenerateToken(userID string) (string, error) {
+	secret := config.GetEnv("JWT_SECRET")
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(48 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
+}
